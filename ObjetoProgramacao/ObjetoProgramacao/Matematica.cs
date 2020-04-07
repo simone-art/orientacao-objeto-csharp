f010b983bc041852438dc7203f1850c6e8aa8a77
simone-art <simoneperiodista3@gmail.com>
2020-04-07 19:40:34 -0300

    Aprendendo sobre objeto em Csharp

---
+++ ObjetoProgramacao/ObjetoProgramacao/Matematica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ObjetoProgramacao
+{
+    class Matematica
+    {
+
+        public int A { get; set;}
+        public int B { get; set; }
+
+        public int Calcular()
+        {
+            return this.A + this.B;
+        }
+        static void Main(string[] args)
+        {
+            Matematica m = new Matematica();
+            m.A = 5;
+            m.B = 10;
+            Console.WriteLine(m.Calcular());
+        }
+    }
+}
